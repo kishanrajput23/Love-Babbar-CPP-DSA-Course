0a00d4c7a2b79e8746b3d1a80cb1cab3a48fc7c1
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-06-13 11:18:04 +0530

    Lecture 7: LeetCode Problem Solving Session

---
+++ Lectures/Lecture_07/Lecture_Codes/1009.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int m = n;
+        int mask = 0;
+
+        if (n==0) {
+            return 1;
+        }
+
+        while (m!=0) {
+            mask = (mask << 1) | 1;
+            m = m >> 1;
+        } 
+
+        return (~n) & mask;
+    }
+};
